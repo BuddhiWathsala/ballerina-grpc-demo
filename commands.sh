3dd494efa8079511012427a9cf259145fb588112
Buddhi <b.wathsala.bw@gmail.com>
2020-08-25 23:18:00 +0530

    Update commands

--- commands.sh
+++ commands.sh
@@ -5,3 +5,7 @@ ballerina grpc --mode client --input protofiles/unary.proto --output src/unary_c
 # Server streaming use case
 ballerina grpc --mode service --input protofiles/s_streaming.proto --output src/s_streaming_server
 ballerina grpc --mode client --input protofiles/s_streaming.proto --output src/s_streaming_client
+
+# Client streaming use case
+ballerina grpc --mode service --input protofiles/c_streaming.proto --output src/c_streaming_server
+ballerina grpc --mode client --input protofiles/c_streaming.proto --output src/c_streaming_client
